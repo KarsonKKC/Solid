fca563fa9a8ec764dda572d8cef28fa5303c9c09
Karson Chow <karsonchow2@hotmail.com>
2022-06-16 16:58:16 +1000

    Revert

--- flask_app.py
+++ flask_app.py
@@ -9,10 +9,3 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello from Karson from UNSW CODE!'
-
-
-fill = input("A Word: ")
-fill_again = input("Another Word: ")
-
-madlib = f"Computer programming is so {fill}! It makes me so excited all the time because I love to {fill_again}!"
-print(madlib)
