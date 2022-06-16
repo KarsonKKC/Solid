4aa21e37e59935771340dca566f55376f9507cda
Karson Chow <karsonchow2@hotmail.com>
2022-06-16 16:59:22 +1000

    Testing 2

--- flask_app.py
+++ flask_app.py
@@ -9,3 +9,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello from Karson from UNSW CODE!'
+
+
+def game_1():
+    fill = input("A Word: ")
+    fill_again = input("Another Word: ")
+
+    madlib = f"Computer programming is so {fill}! It makes me so excited all the time because I love to {fill_again}!"
+    print(madlib)
